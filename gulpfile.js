9c9e1f49b019d3360c32d3325ce708fb8c02f29a
三木 百花 <miki.momoka.0317@gmail.com>
2019-05-06 06:54:27 +0900

    環境更新　gulpなど

--- gulpfile.js
+++ gulpfile.js
@@ -1,144 +1,190 @@
-'use strict';
-/**
- * 
- * gulp v4
- *  
- */
+// miki taskrunner 2019/5/4
+// gulp => 4.0.0
 
-/**
- * 
- * 外部ファイル読み込み
- * 
- */
+// srcフォルダ
+// ・imagemin (圧縮したい画像ファイル) => imgファイルへ
+// ・sass (コンパイルするscssファイル) => cssファイルへ
+
+// minフォルダ
+// ・js (圧縮されたjsファイル)
+// ・css (圧縮されたcssファイル)
+
+// gulp v4からは、taskにコールバックを渡さないとエラーが出てしまう
+
 // gulp
-var gulp = require( 'gulp' );
-// sassの実行ファイル
-var sass = require( 'gulp-sass' );
-// エラー時のデスクトップ通知機能
-var notify = require( 'gulp-notify' );
-// エラー時の強制終了防止
-var plumber = require( 'gulp-plumber' );
-// ソースマップ作成
-var sourcemaps = require( 'gulp-sourcemaps' );
+var gulp = require('gulp');
+
+//
+// sassのコンパイル
+// 
+
+// Sass
+var sass = require('gulp-sass');
 // ベンダープレフィックス
-var pleeease = require( 'gulp-pleeease' );
-// es6
-require( 'es6-promise' ).polyfill();
+var autoprefixer = require("gulp-autoprefixer");
+// watch中エラー防止
+var plumber = require('gulp-plumber');
+// エラーを通知する
+var notify = require("gulp-notify");
+// ソースマップ
+var sourcemaps = require("gulp-sourcemaps");
+// 親scssファイルもコンパイル
+var progeny = require("gulp-progeny");
+// CSSメディアクエリーを整理
+var gcmq = require('gulp-group-css-media-queries');
 
-/**
- * 
- * 設定関係
- * 
- */
-// 実行時の出力ファイルのパス指定
-var cssSrcPath = './src/sass';
-var cssDestPath = './css';
-var jsSrcPath = './src/js';
-var jsDestPath = './js';
-// タスク名
-var task = {
-  sass: 'sass',
-  js: 'js',
-  watch: 'watch'
-};
-
-/**
- * 
- * タスク関係
- * gulp v4からtaskはコールバックを渡してやらないと実行が完了しなくなっている
- * 
- */
-// sass
-gulp.task( 
-  task.sass,
-  function(done){
-    showLog('task : sass');
-    gulp
-      .src( cssSrcPath + '/*.scss' )
-      // ファイル内のエラー検索
-      .pipe(
-        plumber(
-          {
-            errorHandler: notify.onError( 'Error: <%= error.message %>' )
-          }
-        )
-      )
-      // ファイルの圧縮
-      .pipe(
-        sass(
-          {
-            outputStyle: 'expanded',
-            compass: true
-          }
-        )
-        .on( 'error', sass.logError )
-      )
-      // ソースマップの生成
-      .pipe(
-        sourcemaps.write()
-      )
-      // ベンダープレフィックスの生成
-      .pipe(
-        pleeease(
-          {
-            autoprefixer: { 'browsers': ['last 2 versions'] },
-            minifier: false
-          }
-        )
-      )
-      // sassファイルの書き出し
-      .pipe( gulp.dest( cssDestPath ) );
+gulp.task('sass',function(done){
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(progeny())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle:'expanded'
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4']
+    }))
+    .pipe(gcmq())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css/'))
   done();
-  }
-);
-
+})
 
 //
-// JavaScriptファイルのtaskを設定
-//
-gulp.task(
-  task.js,
-  function(done){
-    showLog( 'task : js' );
-    gulp
-      .src( jsSrcPath + '/*.js' )
-      .pipe( gulp . dest( jsDestPath ) );
-    done();
+// 画像の圧縮
+// 
+
+// 外部からダウンロードしてきたファイルは圧縮できなかった
+
+// 書き出し先のファイルと比較し、変更がある場合のみ処理
+var changed = require('gulp-changed');
+// 画像圧縮
+var imagemin = require('gulp-imagemin');
+// jpgファイル
+var imageminJpg = require('imagemin-jpeg-recompress');
+// pngファイル
+var imageminPng = require('imagemin-pngquant');
+// gifファイル
+var imageminGif = require('imagemin-gifsicle');
+// svgファイル
+var svgmin = require('gulp-svgmin');
+
+gulp.task('imagemin', function (done) {
+  // jpeg,png,gif
+  gulp.src('./src/imagemin/*.+(jpg|jpeg|png|gif)')
+    .pipe(changed('./imagemin'))
+    .pipe(imagemin([
+      imageminPng(),
+      imageminJpg(),
+      imageminGif({
+        interlaced: false,
+        optimizationLevel: 3,
+        colors: 180
+      })
+    ]))
+    .pipe(gulp.dest('./img/'));
+  // svg
+  gulp.src('./src/imagemin/*.+(svg)')
+    .pipe(changed('./imagemin'))
+    .pipe(svgmin())
+    .pipe(gulp.dest('./img/'));
+  done();
 });
 
 
 //
-// sassファイルのwatchを設定
+// オートリロード
 //
-gulp.task(
-  task.watch,
-  function(){
-    showLog( 'task : watch' );
-    var target = [
-      cssSrcPath + '/*.scss',
-      jsSrcPath + '/*.js'
-    ];
-    
-    gulp.watch(
-      target,
-      gulp.parallel(task.sass)
-    );
-  }
-);
+
+// 自動的にリロード
+var browserSync = require('browser-sync');
+
+gulp.task('sync', function (done) {
+  browserSync({
+    // server だとphp動かない
+    // server: {
+    //   baseDir: "./",
+    //   // ここ変更
+    //   index: "index.php"
+    // }
+
+    proxy: "localhost/Git/portfolio"
+  });
+  done();
+
+  done();
+});
+gulp.task('bs-reload', function (done) {
+  browserSync.reload();
+  done();
+});
 
 
-// gulpタスクの実行
-gulp.task(
-  'default',
-  gulp.series( gulp.parallel( task.sass, task.js ))
-);
+//
+// ファイル監視
+//
+
+// gulp -v4からは、タスク実行する時に同期処理か非同期処理か明記しないといけない
 
 
+gulp.task('watch',  function (done) {
+  gulp.watch("./**/*.html", gulp.series('bs-reload'));
+  gulp.watch("./**/*.php", gulp.series('bs-reload'));
+  gulp.watch("./src/sass/**/*.scss", gulp.series('sass', 'bs-reload'));
+  gulp.watch("./src/imagemin/*", gulp.series('imagemin', 'bs-reload'));
+  done();
+});
 
-// コンソール関数
-function showLog( message ) {
-  console.log( '# ----------------------------------- #' );
-  console.log( `#    ${message}` );
-  console.log( '# ----------------------------------- #' );
-  
-}
+// gulp を実行で、browserSync立ち上げ + watch実行
+gulp.task('default', gulp.series('sync','watch') );
+
+
+//
+// ファイル圧縮
+//
+
+// JSファイル圧縮
+var uglify = require('gulp-uglify');
+// CSSファイル圧縮
+var cleanCSS = require('gulp-clean-css');
+// ファイル名変更
+var rename = require('gulp-rename');
+
+gulp.task("jsmin", function (done) {
+  gulp.src('./js/*.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./min/js'));
+  done();
+});
+
+gulp.task('cssmin', function (done) {
+  gulp.src("./css/*.css")
+    .pipe(cleanCSS())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('./min/css/'));
+  done();
+});
+
+
+//
+// ES6からES5にコンパイル
+//
+
+var babel = require('gulp-babel');
+
+gulp.task('babel', function (done) {
+  gulp.src('./js/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('./babel/'));
+  done();
+});
